e3d47b4e05e09ae25a5edf1fa0cbbd3a185e0499
Pierre Jacomet <pierrj@hotmail.com>
2014-03-23 20:18:47 -0700

    Fix formatting, make sure it passes pylint better.

--- toto/options.py
+++ toto/options.py
@@ -1,8 +1,9 @@
-from tornado.options import define, options
+from tornado.options import define
 import logging
 
+
 def safe_define(*args, **kwargs):
-  try:
-    define(*args, **kwargs)
-  except Exception as e:
-    logging.error(str(e))
+    try:
+        define(*args, **kwargs)
+    except Exception as e:
+        logging.error(str(e))
