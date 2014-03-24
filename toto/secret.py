d68ca047e6fc71bad8342bac9f9bc34a622615f7
Pierre Jacomet <pierrj@hotmail.com>
2014-03-23 20:31:08 -0700

    Fix formatting, make sure it passes pylint better.

--- toto/secret.py
+++ toto/secret.py
@@ -1,7 +1,9 @@
 from pbkdf2 import crypt
 
+
 def password_hash(secret):
-  return crypt(secret)
+    return crypt(secret)
+
 
 def verify_password(secret, pwhash):
-  return pwhash == crypt(secret, pwhash)
+    return pwhash == crypt(secret, pwhash)
